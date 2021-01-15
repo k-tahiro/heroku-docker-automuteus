7dfeb3ef12f1a3ac3bc8c448fa72cef14cb4cfdb
Keisuke Hirota <tahiro.k.ad@gmail.com>
2021-01-16 00:18:08 +0900

    Change CMD

---
+++ web/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -ex
+
+export POSTGRES_ADDR="$(echo ${DATABASE_URL} | cut -d@ -f2)"
+export POSTGRES_USER="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)"
+export POSTGRES_PASS="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
+export REDIS_ADDR="$(echo ${REDIS_URL} | cut -d@ -f2)"
+export REDIS_PASS="$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)"
+
+/usr/bin/supervisord -c /etc/supervisord.conf
