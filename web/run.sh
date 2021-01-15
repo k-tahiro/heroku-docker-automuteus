44ec910587dbd7414687d1d2af208e5cb44cc0a7
Keisuke Hirota <tahiro.k.ad@gmail.com>
2021-01-16 00:31:34 +0900

    Add HOST envvar

--- web/run.sh
+++ web/run.sh
@@ -6,5 +6,6 @@ export POSTGRES_USER="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -
 export POSTGRES_PASS="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
 export REDIS_ADDR="$(echo ${REDIS_URL} | cut -d@ -f2)"
 export REDIS_PASS="$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)"
+export HOST="https://${HEROKU_APP_NAME}.herokuapp.com"
 
 /usr/bin/supervisord -c /etc/supervisord.conf
