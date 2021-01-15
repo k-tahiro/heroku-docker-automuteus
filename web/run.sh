c1c6d8869d3baeb5d71268c0ad6a98f4dad7cf4c
Keisuke Hirota <tahiro.k.ad@gmail.com>
2021-01-16 08:53:06 +0900

    Refactor

--- web/run.sh
+++ web/run.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -ex
 
+export HOST="$(heroku info -s | grep web_url | cut -d= -f2)"
 export POSTGRES_ADDR="$(echo ${DATABASE_URL} | cut -d@ -f2)"
 export POSTGRES_USER="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)"
 export POSTGRES_PASS="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
 export REDIS_ADDR="$(echo ${REDIS_URL} | cut -d@ -f2)"
 export REDIS_PASS="$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)"
-export HOST="https://${HEROKU_APP_NAME}.herokuapp.com"
 
 /usr/bin/supervisord -c /etc/supervisord.conf
