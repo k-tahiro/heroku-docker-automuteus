b4eeeff2280056999cd351dda4fa9dc485b50d17
Keisuke Hirota <tahiro.k.ad@gmail.com>
2021-01-17 12:33:09 +0900

    Refactor

--- app/set_config.sh
+++ app/set_config.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -ex
 
-heroku config:set HOST="$(heroku info -s | grep web_url | cut -d= -f2)"
-heroku config:set POSTGRES_ADDR="$(echo ${DATABASE_URL} | cut -d@ -f2)"
-heroku config:set POSTGRES_USER="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)"
-heroku config:set POSTGRES_PASS="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
-heroku config:set REDIS_ADDR="$(echo ${REDIS_URL} | cut -d@ -f2)"
-heroku config:set REDIS_PASS="$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)"
+heroku config:set "HOST=$(heroku info -s | grep web_url | cut -d= -f2)"
+heroku config:set "POSTGRES_ADDR=$(echo ${DATABASE_URL} | cut -d@ -f2)"
+heroku config:set "POSTGRES_USER=$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)"
+heroku config:set "POSTGRES_PASS=$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
+heroku config:set "REDIS_ADDR=$(echo ${REDIS_URL} | cut -d@ -f2)"
+heroku config:set "REDIS_PASS=$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)"
