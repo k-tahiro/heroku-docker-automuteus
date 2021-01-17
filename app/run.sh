8c18efeb25158bf801f41ee92d549d746acf98d8
Keisuke Hirota <tahiro.k.ad@gmail.com>
2021-01-17 12:05:32 +0900

    Add release phase

--- app/run.sh
+++ app/run.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 set -ex
 
-export POSTGRES_ADDR="$(echo ${DATABASE_URL} | cut -d@ -f2)"
-export POSTGRES_USER="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)"
-export POSTGRES_PASS="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
-export REDIS_ADDR="$(echo ${REDIS_URL} | cut -d@ -f2)"
-export REDIS_PASS="$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)"
-
-export HOST="https://${HEROKU_APP_NAME}.herokuapp.com"
-
-/usr/bin/supervisord -c /etc/supervisord.conf
+heroku config:set HOST="$(heroku info -s | grep web_url | cut -d= -f2)"
+heroku config:set POSTGRES_ADDR="$(echo ${DATABASE_URL} | cut -d@ -f2)"
+heroku config:set POSTGRES_USER="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)"
+heroku config:set POSTGRES_PASS="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
+heroku config:set REDIS_ADDR="$(echo ${REDIS_URL} | cut -d@ -f2)"
+heroku config:set REDIS_PASS="$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)"
