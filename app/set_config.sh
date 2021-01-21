529d35b012b3668a23dedf23565fc469dc6eb0ed
Keisuke Hirota <tahiro.k.ad@gmail.com>
2021-01-21 13:16:14 +0900

    Fix cmd

--- app/set_config.sh
+++ app/set_config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-heroku config:set "HOST=$(heroku info -s | grep web_url | cut -d= -f2)"
+heroku config:set "HOST=$(heroku apps:info -s | grep web_url | cut -d= -f2)"
 heroku config:set "POSTGRES_ADDR=$(echo ${DATABASE_URL} | cut -d@ -f2)"
 heroku config:set "POSTGRES_USER=$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)"
 heroku config:set "POSTGRES_PASS=$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
