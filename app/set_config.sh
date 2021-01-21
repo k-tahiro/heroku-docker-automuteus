26144a9780ff4f8c8021960ebd32c5219c8a5701
Keisuke Hirota <tahiro.k.ad@gmail.com>
2021-01-21 13:53:15 +0900

    Get APP_ID from HEROKU_RELEASE_STREAM_URL

--- app/set_config.sh
+++ app/set_config.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -ex
 
-heroku config:set "HOST=$(heroku apps:info -s | grep web_url | cut -d= -f2)"
-heroku config:set "POSTGRES_ADDR=$(echo ${DATABASE_URL} | cut -d@ -f2)"
-heroku config:set "POSTGRES_USER=$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)"
-heroku config:set "POSTGRES_PASS=$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
-heroku config:set "REDIS_ADDR=$(echo ${REDIS_URL} | cut -d@ -f2)"
-heroku config:set "REDIS_PASS=$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)"
+APP_ID="$(echo "${HEROKU_RELEASE_STREAM_URL}" | cut -d/ -f6)"
+
+heroku config:set "HOST=$(heroku apps:info -s -a ${APP_ID} | grep web_url | cut -d= -f2)" -a "${APP_ID}"
+heroku config:set "POSTGRES_ADDR=$(echo ${DATABASE_URL} | cut -d@ -f2)" -a "${APP_ID}"
+heroku config:set "POSTGRES_USER=$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)" -a "${APP_ID}"
+heroku config:set "POSTGRES_PASS=$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)" -a "${APP_ID}"
+heroku config:set "REDIS_ADDR=$(echo ${REDIS_URL} | cut -d@ -f2)" -a "${APP_ID}"
+heroku config:set "REDIS_PASS=$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)" -a "${APP_ID}"
