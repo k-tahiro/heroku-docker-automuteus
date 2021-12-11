76cc0b89f7b79d85610353eb838db517a1d5e46d
Keisuke Hirota <tahiro.k.ad@gmail.com>
2021-12-12 02:08:13 +0900

    Bug fix

--- release/run.sh
+++ release/run.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -ex
 
-env
+APP_ID="$(echo ${HEROKU_RELEASE_STREAM_URL} | cut -d/ -f6)"
 
-export APP_ID="$(echo ${HEROKU_RELEASE_STREAM_URL} | cut -d/ -f6)"
-export POSTGRES_ADDR="$(echo ${DATABASE_URL} | cut -d@ -f2)"
-export POSTGRES_USER="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)"
-export POSTGRES_PASS="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
-export REDIS_ADDR="$(echo ${REDIS_URL} | cut -d@ -f2)"
-export REDIS_PASS="$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)"
--- web/run.sh
+++ web/run.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -ex
 
-env
+export HOST="https://${HEROKU_APP_NAME}.herokuapp.com"
 
-export HOST="https://${APP_ID}.herokuapp.com"
-/usr/bin/supervisord -c /etc/supervisord.conf
+export POSTGRES_ADDR="$(echo ${DATABASE_URL} | cut -d@ -f2)"
+export POSTGRES_USER="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)"
+export POSTGRES_PASS="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
+export REDIS_ADDR="$(echo ${REDIS_URL} | cut -d@ -f2)"
+export REDIS_PASS="$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)"
+/usr/bin/supervisord -c supervisord.conf
