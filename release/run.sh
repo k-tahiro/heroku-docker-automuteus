9e877ee53438bae1e80490cfba3fd6b761f49341
Keisuke Hirota <tahiro.k.ad@gmail.com>
2021-12-12 02:01:00 +0900

    Refactor

--- release/run.sh
+++ release/run.sh
@@ -3,11 +3,9 @@ set -ex
 
 env
 
-APP_ID="$(echo ${HEROKU_RELEASE_STREAM_URL} | cut -d/ -f6)"
-
-heroku config:set "HOST=$(heroku apps:info -s -a ${APP_ID} | grep web_url | cut -d= -f2)" -a "${APP_ID}"
-heroku config:set "POSTGRES_ADDR=$(echo ${DATABASE_URL} | cut -d@ -f2)" -a "${APP_ID}"
-heroku config:set "POSTGRES_USER=$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)" -a "${APP_ID}"
-heroku config:set "POSTGRES_PASS=$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)" -a "${APP_ID}"
-heroku config:set "REDIS_ADDR=$(echo ${REDIS_URL} | cut -d@ -f2)" -a "${APP_ID}"
-heroku config:set "REDIS_PASS=$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)" -a "${APP_ID}"
+export APP_ID="$(echo ${HEROKU_RELEASE_STREAM_URL} | cut -d/ -f6)"
+export POSTGRES_ADDR="$(echo ${DATABASE_URL} | cut -d@ -f2)"
+export POSTGRES_USER="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f2 | cut -d/ -f3)"
+export POSTGRES_PASS="$(echo ${DATABASE_URL} | cut -d@ -f1 | cut -d: -f3)"
+export REDIS_ADDR="$(echo ${REDIS_URL} | cut -d@ -f2)"
+export REDIS_PASS="$(echo ${REDIS_URL} | cut -d@ -f1 | cut -d: -f3)"
